0de42de0d40c2471cfcd5cba3c3280c1f0572d95
Felix Gordon <78864962+fxGRDN@users.noreply.github.com>
2022-01-27 23:20:15 +0100

    fix

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,7 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: node({env: { port: process|.env.PORT }}),
+		adapter: node(),
 	}
 };
 
